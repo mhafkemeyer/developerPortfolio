6c6d48d040d1584cdb25ebfe94142d95cc26a7fd
McKenzie Hafkemeyer <mckenzie.hafkemeyer@gmail.com>
2018-08-30 12:40:24 -0500

    Contact form is wired - sending emails

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var nodemailer = require('nodemailer');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +20,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
     res.render('index', {title: 'Portfolio'});
+});
+
+// POST route from contact form
+app.post('/contact', function (req, res) {
+    var mailOpts, smtpTrans;
+    smtpTrans = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+    mailOpts = {
+        from: process.env.GMAIL_USER,
+        to: process.env.GMAIL_USER,
+        subject: 'New message from contact form',
+        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    };
+    smtpTrans.sendMail(mailOpts, function (error, response) {
+        if (error) {
+            console.log(error);
+            res.render('index');
+        }
+        else {
+            res.render('index');
+        }
+    });
 });
 
 // catch 404 and forward to error handler
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,32 @@
 function scrollToContent(area){
     document.getElementById(area).scrollIntoView(true);
-    //document.getElementById(area).classList.add("active");
-    $()
 }
 
 $(window).scroll(function(){
-    if ($(window).scrollTop() > 500)
+    var scrollTop = $(window).scrollTop();
+
+    /* Determine if menu should be fixed to the top of the screen */
+    if (scrollTop > 500)
         $('.top-nav').addClass("fixed");
     else
         $('.top-nav').removeClass("fixed");
+
+    /* Determine which, if any, of the menu options is active */
+    $('.top-nav').find('*').removeClass('active');
+    if (scrollTop < $('#about-container').offset().top){
+        $('.top-nav').find('*').removeClass('active');
+    }
+    else if (scrollTop < $('#exp-container').offset().top) {
+        $('#about-menu').addClass('active');
+    }
+    else if (scrollTop < $('#edu-container').offset().top) {
+        $('#exp-menu').addClass('active');
+    }
+    else if (scrollTop < $('#contact-container').offset().top) {
+        $('#edu-menu').addClass('active');
+    }
+    else if (scrollTop >= $('#contact-container').offset().top) {
+        $('#contact-menu').addClass('active');
+    }
+
 });
